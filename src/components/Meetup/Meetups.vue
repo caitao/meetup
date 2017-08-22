26c64a528b2d7a7c1828c85cb2c981e29ba26bd1
caitao <caotao@jx.chinamobile.com>
2017-08-22 09:57:33 +0800

    change view of meetups.vue

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,8 +1,26 @@
 <template lang="html">
-    <v-container row wrap>
-      <v-flex xs12>
-
-      </v-flex>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card class="info">
+            <v-container fluid>
+              <v-layout row>
+                <v-flex xs5 sm4 md3>
+                  <v-card-media
+                    src="../../static/doc-images/carousel/beijing.jpg"
+                    height="120px"
+                  ></v-card-media>
+                </v-flex>
+                <v-flex xs7 sm8 md9>
+                  <v-card-title primary-title>
+                    <div>My Meetup</div>
+                  </v-card-title>
+                </v-flex>
+              </v-layout>
+            </v-container>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import App from './App'
 import router from './router'
 
 Vue.use(Vuetify)
-Vue.config.productionTip = false  
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
