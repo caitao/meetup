184bf26e7c3c19a7f187bd99995d8cd2b53698aa
caitao <caotao@jx.chinamobile.com>
2017-08-30 18:06:31 +0800

    Vuex add in . mkdir store  and new file index.js

---
+++ src/store/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export const store = new Vuex.store({
+  state: {
+    loadedMeetups: [
+      {src: '../../static/doc-images/carousel/beijing.jpg', id: 'sadfasdfw21', title: 'Meetup In BeiJing', date: '2017-08-23'},
+      {src: '../../static/doc-images/carousel/shanghai.jpg', id: 'wqjkpsck23', title: 'Meetup In ShangHai', date: '2017-08-24'},
+      {src: '../../static/doc-images/carousel/shengzhen.jpg', id: 'kapadsap13', title: 'Meetup In ShengZhen', date: '2017-08-25'},
+      {src: '../../static/doc-images/carousel/hongkong.jpg', id: 'skjpqjwk42', title: 'Meetup In HongKong', date: '2017-08-27'}
+    ],
+    user: {
+      id: 'asdkfswq12',
+      registeredMeetups: ['skjpqjwk42']
+    }
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+    loadedMeetups (state) {
+      return state.loadedMeetups.sort((meetupA,meetupB) => {
+        return meetupA.date > meetupB.date
+      })
+    },
+    loadedMeetup (state) {
+      return (meetupId) => {
+        return state.loadedMeetups.find((meetup) => {
+          return meetup.id === meetupId
+        })
+      }
+    }
+  }
+})
