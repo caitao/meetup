bae16327740a4c8087057e65740b8add8be617e3
caitao <caotao@jx.chinamobile.com>
2017-09-13 17:34:17 +0800

    import wilddog compunent and create signup.vue view

--- src/components/User/SignUp.vue
+++ src/components/User/SignUp.vue
@@ -1,11 +1,89 @@
 <template lang="html">
-  <div class="content">
-    <p>SIGN UP</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4>Sign UP</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form @submit.prevent="signup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="email"
+                label="Email"
+                id="email"
+                v-model="email"
+                type="email"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="password"
+                label="Password"
+                id="password"
+                v-model="password"
+                type="password"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="comfirmPassword"
+                label="Comfirm Password"
+                id="comfirmPassword"
+                v-model="comfirmPassword"
+                type="password">
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn
+              class="primary"
+              :disabled="!formIsValid"
+              type="submit">Sign Up</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    formIsValid () {
+      return this.email !== '' &&
+        this.password !== ''
+    }
+  },
+  methods: {
+    onCreateMeetup () {
+      const meetupData = {
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+        date: this.submitableDateTime
+      }
+      this.$store.dispatch('createMeetup', meetupData)
+      this.$router.push('/meetups')
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
+import * as wilddog from 'wilddog'
+import * as WildVue from 'wildvue'
 import App from './App'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
 
 Vue.use(Vuetify)
+Vue.use(WildVue)
 Vue.config.productionTip = false
 Vue.filter('dateFilter', DateFilter)
 
@@ -14,5 +17,11 @@ new Vue({
   el: '#app',
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    wilddog.initializeApp({
+      syncURL: 'https://wd9504485366mhjfht.wilddogio.com',
+      authDomain: 'wd9504485366mhjfht.wilddog.com'
+    })
+  }
 })
