a37aa598eefbbcd127b5ea834579b540e6dbd260
caitao <caotao@jx.chinamobile.com>
2017-10-08 23:59:49 +0800

    try modify the auth-guard.js to judge  if wilddog.session.signIn is true to avoid the issue of refresh time store.getter.user is null

--- src/router/auth-guard.js
+++ src/router/auth-guard.js
@@ -1,11 +1,14 @@
 import { store } from '../store'
+import * as wilddog from 'wilddog'
 
 export default (to, from, next) => {
   if (store.getters.user) {
     next()
     console.log(store.getters.user)
+  } else if (wilddog.session.signIn) {
+    next()
+    console.log(store.getters.user)
   } else {
     next('/signin')
-    console.log(store.getters.user)
   }
 }
